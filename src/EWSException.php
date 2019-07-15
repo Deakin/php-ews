e6642ef70f2c44a61c276d76d67d475dca8324c6
isha <isha.nagpal@deakin.edu.au>
2019-07-15 14:52:07 +1000

    added Exception Class

---
+++ src/EWSException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Exception class for Exchange Web Services
+ *
+ * @package php-ews
+ * @subpackage Exception
+ */
+namespace PhpEws;
+use Exception;
+/**
+ * Exception class for Exchange Web Services
+ */
+class EWSException extends Exception
+{
+}
